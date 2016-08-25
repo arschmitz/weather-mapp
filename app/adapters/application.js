1cf31bcb639c93d02bd0f4a21e6df732e550af85
Alexander Schmitz <arschmitz@gmail.com>
2016-08-25 19:43:42 -0400

    Weathermap: stop using owm cache server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api/',
-  host: 'http://owm.arschmitz.me?url=https://api.wunderground.com',
+  host: 'https://api.wunderground.com',
   buildURL(model, id) {
     let key = localStorage.apikey || 'e7c863ca90d971b4';
     if (/forecast/.test(model)) {
